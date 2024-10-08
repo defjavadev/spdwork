06e41da6032c126aad523fec975048b3d2bfc71c
defjavadev <zhuabekov.damir.1705@gmail.com>
2024-10-08 15:34:22 +0500

    new func

--- src/V8Engine.java
+++ src/V8Engine.java
@@ -2,4 +2,8 @@ public class V8Engine implements Engine {
     public void describe() {
         System.out.println("This is a V8 engine.");
     }
+
+    void test() {
+        System.out.println("Hello World");
+    }
 }
